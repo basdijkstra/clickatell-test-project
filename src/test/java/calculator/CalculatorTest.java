b201d9a6efabac624af45b5e0f032155d498b6ca
Bas Dijkstra <bas@ontestautomation.com>
2022-06-13 16:08:53 +0200

    Let's fix the test again

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -12,7 +12,7 @@ public class CalculatorTest {
 
         return new Object[][] {
                 {2, 2, 4},
-                {1, 1, 3},
+                {1, 1, 2},
                 {5, 4, 9}
         };
     }
